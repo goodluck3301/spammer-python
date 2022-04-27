1cfc470863789dc555291886e41a7cb6a756af0b
Levon <100533325+goodluck3301@users.noreply.github.com>
2022-04-27 22:48:53 +0400

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+from termcolor import colored
+
+def spam():
+    text = input(colored('Enter text for send!\n   ', 'green'))
+    count = input(colored('Enter count!\n   ', 'green'))
+    print(colored('Click on \'Write a message...\'', 'red'))
+    print(colored('    For it you have 5 seconds', 'red'))
+    time.sleep(5)
+    print(colored('If all is right, spam started ;)', 'green'))
+
+    try:
+        for i in range(int(count)):
+            pyautogui.typewrite(text)
+            pyautogui.press('enter')
+    except:
+        print(colored('    Input error:(', 'red'))
+
+
+def main():
+    spam()
+
+if __name__ == '__main__':
+    main()
+
